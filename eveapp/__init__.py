79a3eb9d1994ae5f031d735b77d4a97168d01a57
Harry Reeder <harry@harryreeder.co.uk>
2014-03-21 22:54:54 -0400

    Add celery settings

--- eveapp/__init__.py
+++ eveapp/__init__.py
@@ -3,7 +3,20 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
+from celery import Celery
 from config import basedir
+
+def make_celery(app):
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -12,5 +25,6 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+celery = make_celery(app)
 
 from eveapp import views, filters, models
