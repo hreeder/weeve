32413772400812e01cb838b14e9cd15b600d608c
Harry Reeder <harry@harryreeder.co.uk>
2014-03-24 14:09:40 -0400

    Nicknames don't need to be unique any mroe

--- eveapp/models.py
+++ eveapp/models.py
@@ -6,7 +6,7 @@ ROLE_ADMIN = 1
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(64), index=True, unique=True)
+    nickname = db.Column(db.String(64), index=True)
     email = db.Column(db.String(120), index=True, unique=True)
     role = db.Column(db.SmallInteger, default=ROLE_USER)
     keys = db.relationship('Key', backref='owner', lazy='dynamic')
